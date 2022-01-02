cddf2c9b7777ac0b1f809cfce806a127e7ed46f2
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 02:21:14 +0200

    Connect the three fragments with the main activity and with the bottom navigation

--- app/src/main/java/com/mohdabbas/weatherapp/MainActivity.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mohdabbas.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.mohdabbas.weatherapp.favcities.FavoriteCitiesFragment
+import com.mohdabbas.weatherapp.home.HomeFragment
+import com.mohdabbas.weatherapp.settings.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavigationView()
     }
+
+    private fun setupBottomNavigationView() {
+        val homeFragment = HomeFragment()
+        val favCitiesFragment = FavoriteCitiesFragment()
+        val settingsFragment = SettingsFragment()
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.fav_cities -> setCurrentFragment(favCitiesFragment)
+                R.id.settings -> setCurrentFragment(settingsFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, fragment)
+                commit()
+            }
 }
