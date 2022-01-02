7f074296c54a32e88d07d4d899cd9f7e1dd97573
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 19:02:22 +0200

    Create city weather details acitvity

---
+++ app/src/main/java/com/mohdabbas/weatherapp/ui/details/CityWeatherDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.mohdabbas.weatherapp.ui.details
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mohdabbas.weatherapp.R
+
+class CityWeatherDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_weather_details)
+    }
+}
