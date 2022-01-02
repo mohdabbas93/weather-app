0fce95670e3386e66434f8789b986d9f8dc931fa
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 16:26:29 +0200

    Create and adapter for the favorite cities and setup it in the fav cities fragmenet with dummy data

---
+++ app/src/main/java/com/mohdabbas/weatherapp/favcities/FavoriteCitiesAdapter.kt
@@ -0,0 +1,75 @@
+package com.mohdabbas.weatherapp.favcities
+
+import android.content.Context
+import android.location.Geocoder
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mohdabbas.weatherapp.R
+import com.mohdabbas.weatherapp.data.source.local.FavoriteCity
+import kotlinx.android.synthetic.main.item_fav_city.view.*
+import java.io.IOException
+import java.util.*
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/2/22.
+ */
+class FavoriteCitiesAdapter(private var data: List<FavoriteCity>) :
+    RecyclerView.Adapter<FavoriteCitiesAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val weatherConditionIcon: ImageView = view.weatherConditionIcon
+        val currentTempTextView: TextView = view.currentTempTextView
+        val weatherConditionTextView: TextView = view.weatherConditionTextView
+        val cityNameTextView: TextView = view.cityNameTextView
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_fav_city, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        Glide.with(viewHolder.itemView.context)
+            .load("http://openweathermap.org/img/wn/${data[position].weatherConditionIcon}@2x.png")
+            .centerCrop()
+            .into(viewHolder.weatherConditionIcon)
+
+        viewHolder.weatherConditionTextView.text = data[position].weatherCondition
+
+        viewHolder.currentTempTextView.text = viewHolder.itemView.context.getString(
+            R.string.current_temp,
+            data[position].currentTemperature.toInt()
+        )
+
+        viewHolder.cityNameTextView.text = getCityNameAndCountry(
+            viewHolder.itemView.context,
+            data[position].lat,
+            data[position].lng
+        )
+    }
+
+    override fun getItemCount() = data.size
+
+    private fun getCityNameAndCountry(
+        context: Context,
+        lat: Double,
+        lng: Double
+    ): String {
+        try {
+            val addresses = Geocoder(context, Locale.getDefault()).getFromLocation(lat, lng, 1)
+            return "${addresses[0].adminArea}, ${addresses[0].countryName}"
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return ""
+    }
+}
--- app/src/main/java/com/mohdabbas/weatherapp/favcities/FavoriteCitiesFragment.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/favcities/FavoriteCitiesFragment.kt
@@ -1,11 +1,13 @@
 package com.mohdabbas.weatherapp.favcities
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.mohdabbas.weatherapp.R
+import com.mohdabbas.weatherapp.data.source.local.FavoriteCity
+import kotlinx.android.synthetic.main.fragment_favorite_cities.*
 
 class FavoriteCitiesFragment : Fragment() {
 
@@ -19,5 +21,26 @@ class FavoriteCitiesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite_cities, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupFavoriteCitiesRecyclerView()
+    }
+
+    private var adapter: FavoriteCitiesAdapter? = null
+
+    private fun setupFavoriteCitiesRecyclerView() {
+        adapter = FavoriteCitiesAdapter(
+            listOf(
+                FavoriteCity(1, 24.5, 53.4, 34.0, "Sunny", "0d2", 13.0, 34, 1234.0),
+                FavoriteCity(1, 24.5, 53.4, 34.0, "Sunny", "0d2", 13.0, 34, 1234.0),
+                FavoriteCity(1, 24.5, 53.4, 34.0, "Sunny", "0d2", 13.0, 34, 1234.0),
+                FavoriteCity(1, 24.5, 53.4, 34.0, "Sunny", "0d2", 13.0, 34, 1234.0),
+                FavoriteCity(1, 24.5, 53.4, 34.0, "Sunny", "0d2", 13.0, 34, 1234.0)
+            )
+        )
+        favCitiesRecyclerView.adapter = adapter
     }
 }
