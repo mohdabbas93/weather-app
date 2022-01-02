1e25c7771de6a749eabc22b1cf97e671c52bf99e
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 02:52:12 +0200

    Create a weather api using retrofit with an open weather map api along with dto classes to hold the received data

---
+++ app/src/main/java/com/mohdabbas/weatherapp/data/source/remote/WeatherApi.kt
@@ -0,0 +1,43 @@
+package com.mohdabbas.weatherapp.data.source.remote
+
+import android.util.Log
+import com.mohdabbas.weatherapp.data.source.remote.dto.CityWeatherDto
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/2/22.
+ */
+interface WeatherApi {
+    @GET("onecall?exclude=hourly,daily&appid=$API_KEY")
+    suspend fun getCityWeatherData(
+        @Query("lat") lat: Double,
+        @Query("lon") lng: Double
+    ): CityWeatherDto
+
+    companion object {
+        private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+        const val API_KEY = "f908845614434e18d389e8a0760e100c"
+
+        fun create(): WeatherApi {
+            val logger = HttpLoggingInterceptor { Log.d("API", it) }
+            logger.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherApi::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/mohdabbas/weatherapp/data/source/remote/dto/CityWeatherDto.kt
@@ -0,0 +1,74 @@
+package com.mohdabbas.weatherapp.data.source.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/1/22.
+ */
+
+data class CityWeatherDto(
+    @SerializedName("lat")
+    val lat: Double,
+    @SerializedName("lon")
+    val lng: Double,
+    @SerializedName("timezone")
+    val timezone: String,
+    @SerializedName("current")
+    val currentWeather: CurrentWeatherDto,
+    @SerializedName("hourly")
+    val hourlyWeather: List<HourlyWeatherDto>,
+    @SerializedName("daily")
+    val dailyWeather: List<DailyWeatherDto>
+)
+
+data class CurrentWeatherDto(
+    @SerializedName("dt")
+    val currentUTCTime: Long,
+    @SerializedName("temp")
+    val temperature: Double,
+    @SerializedName("feels_like")
+    val feelsLike: Double,
+    @SerializedName("pressure")
+    val pressure: Long,
+    @SerializedName("humidity")
+    val humidity: Int,
+    @SerializedName("wind_speed")
+    val windSpeed: Double,
+    @SerializedName("weather")
+    val weather: List<WeatherDto>
+)
+
+data class WeatherDto(
+    @SerializedName("main")
+    val weatherCondition: String,
+    @SerializedName("icon")
+    val icon: String
+)
+
+data class HourlyWeatherDto(
+    @SerializedName("dt")
+    val currentUTCTime: Long,
+    @SerializedName("temp")
+    val temperature: Double,
+    @SerializedName("humidity")
+    val humidity: Double,
+    @SerializedName("weather")
+    val weather: WeatherDto
+)
+
+data class DailyWeatherDto(
+    @SerializedName("dt")
+    val currentUTCTime: Long,
+    @SerializedName("weather")
+    val weather: WeatherDto,
+    @SerializedName("temp")
+    val temperature: TemperatureDto
+)
+
+data class TemperatureDto(
+    @SerializedName("min")
+    val minTemperature: Double,
+    @SerializedName("max")
+    val maxTemperature: Double
+)
