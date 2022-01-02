dfaf54582074c1b1ccd158cf7ccc089abc105ad1
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 17:22:57 +0200

    create a persistence manager

---
+++ app/src/main/java/com/mohdabbas/weatherapp/persistence/PersistenceManager.kt
@@ -0,0 +1,23 @@
+package com.mohdabbas.weatherapp.persistence
+
+import android.content.Context
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/2/22.
+ */
+class PersistenceManager(context: Context) {
+
+    private var sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
+    private val editor = sharedPreferences.edit()
+
+    var isCelsius: Boolean
+        get() = sharedPreferences.getBoolean(IS_CELSIUS, true)
+        set(value) = editor.putBoolean(IS_CELSIUS, value).apply()
+
+    companion object {
+        private const val PREF_NAME = "WeatherSharedPref"
+        private const val IS_CELSIUS = "IS_CELSIUS"
+    }
+}
