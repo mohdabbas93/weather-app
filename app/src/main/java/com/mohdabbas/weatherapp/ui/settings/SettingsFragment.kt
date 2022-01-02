76e7be11e2ba574b08fec340f7c453b74b20562e
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 18:00:25 +0200

    Setup temperature unit selector

--- app/src/main/java/com/mohdabbas/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/ui/settings/SettingsFragment.kt
@@ -1,16 +1,41 @@
 package com.mohdabbas.weatherapp.ui.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.mohdabbas.weatherapp.R
+import com.mohdabbas.weatherapp.persistence.PersistenceManager
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
 
+    // Bad idea, will refactor later (Maybe use view model factory or DI)
+    private lateinit var persistenceManager: PersistenceManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        persistenceManager = PersistenceManager(requireContext())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRadioButtons()
+    }
+
+    private fun setupRadioButtons() {
+        if (persistenceManager.isCelsius) {
+            celsiusRadioButton.isChecked = true
+        } else {
+            fahrenheitRadioButton.isChecked = true
+        }
+
+        tempUnitRadioGroup.setOnCheckedChangeListener { _, i ->
+            persistenceManager.isCelsius = celsiusRadioButton.id == i
+        }
     }
 
     override fun onCreateView(
