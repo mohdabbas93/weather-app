d81e4eed6245f8e080cf2fa5c122dade4e69af50
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 02:55:21 +0200

    Create an interface for the weather data source to act as a single entry point to access the weather data

---
+++ app/src/main/java/com/mohdabbas/weatherapp/data/source/WeatherDataSource.kt
@@ -0,0 +1,15 @@
+package com.mohdabbas.weatherapp.data.source
+
+import com.mohdabbas.weatherapp.data.source.remote.dto.CityWeatherDto
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/2/22.
+ */
+
+/**
+ * Main entry points for accessing weather data.
+ */
+interface WeatherDataSource {
+    suspend fun getWeatherData(lat: Double, lng: Double): CityWeatherDto
+}
