9ceef8f9fdd223c33e053f72c76802833227ef36
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-02 15:30:55 +0200

    Create favortie_cities entities and add it to the database

--- app/src/main/java/com/mohdabbas/weatherapp/data/source/local/WeatherDatabase.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/data/source/local/WeatherDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
  * Created by Mohammad Abbas
  * On: 1/1/22.
  */
-@Database(entities = [CityWeather::class], version = 1)
+@Database(entities = [CityWeather::class, FavoriteCity::class], version = 1)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
