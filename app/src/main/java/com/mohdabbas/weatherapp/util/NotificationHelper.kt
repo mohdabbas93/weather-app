e24e0631206088203b6f4d39abad836be3da5ba3
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-03 04:20:23 +0200

    Create a helper class to deal with the notifications

---
+++ app/src/main/java/com/mohdabbas/weatherapp/util/NotificationHelper.kt
@@ -0,0 +1,49 @@
+package com.mohdabbas.weatherapp.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.mohdabbas.weatherapp.R
+
+/**
+ * Created by Mohammad Abbas
+ * On: 1/3/22.
+ */
+class NotificationHelper(private val context: Context) {
+
+    init {
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    fun createNotification() {
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(context.getString(R.string.temp_today_title))
+            .setContentText(context.getString(R.string.temp_today_text, 34))
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        val manager = NotificationManagerCompat.from(context)
+        manager.notify(0, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "1001"
+        private const val CHANNEL_NAME = "WeatherNotification"
+    }
+}
