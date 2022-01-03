ac32c6a06ebb33ee5e97dd8661cdddef1b2b3485
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-03 02:07:21 +0200

    Open the home fragement in the details activity and pas the lat and lng to it

--- app/src/main/java/com/mohdabbas/weatherapp/ui/details/CityWeatherDetailsActivity.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/ui/details/CityWeatherDetailsActivity.kt
@@ -3,10 +3,26 @@ package com.mohdabbas.weatherapp.ui.details
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mohdabbas.weatherapp.R
+import com.mohdabbas.weatherapp.ui.home.HomeFragment
 
 class CityWeatherDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_weather_details)
+
+        val lat = intent.getDoubleExtra("lat", 0.0)
+        val lng = intent.getDoubleExtra("lng", 0.0)
+
+        val homeFragment = HomeFragment().apply {
+            arguments = Bundle().apply {
+                putDouble("lat", lat)
+                putDouble("lng", lng)
+            }
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, homeFragment)
+            commit()
+        }
     }
 }
