cb950e62820749830f178dd9d273e81d6da8237b
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-04 22:21:35 +0200

    Add DailyWeather entity to the database

--- app/src/main/java/com/mohdabbas/weatherapp/data/source/local/WeatherDatabase.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/data/source/local/WeatherDatabase.kt
@@ -2,12 +2,13 @@ package com.mohdabbas.weatherapp.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.mohdabbas.weatherapp.data.source.local.entity.DailyWeather
 
 /**
  * Created by Mohammad Abbas
  * On: 1/1/22.
  */
-@Database(entities = [CityWeather::class, FavoriteCity::class], version = 1)
+@Database(entities = [CityWeather::class, DailyWeather::class, FavoriteCity::class], version = 1)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
