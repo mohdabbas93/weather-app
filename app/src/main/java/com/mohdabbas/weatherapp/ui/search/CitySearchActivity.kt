ff8c2eadd4e17fdc60cd1ec042bfb1f2f0536de1
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-05 08:15:42 +0200

    Search for the search input text  when click search

--- app/src/main/java/com/mohdabbas/weatherapp/ui/search/CitySearchActivity.kt
+++ app/src/main/java/com/mohdabbas/weatherapp/ui/search/CitySearchActivity.kt
@@ -1,12 +1,29 @@
 package com.mohdabbas.weatherapp.ui.search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.mohdabbas.weatherapp.R
+import com.mohdabbas.weatherapp.WeatherApplication
+import kotlinx.android.synthetic.main.activity_city_search.*
 
 class CitySearchActivity : AppCompatActivity() {
+
+    // TODO: Very stupid way to instantiate a view model, for it works for now (refactor later)
+    private val viewModel = CitySearchViewModel(WeatherApplication.citySearchRepository)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_search)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        searchButton.setOnClickListener {
+            val searchTerm = citySearchInputEditText.text.toString().trim()
+            if (searchTerm.isNotEmpty()) {
+                viewModel.searchCity(searchTerm)
+            }
+        }
     }
 }
