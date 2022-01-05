d89067c9e741643ce903f51b18b1d3410d95b8f0
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-05 07:29:42 +0200

    Create City search activity page

---
+++ app/src/main/java/com/mohdabbas/weatherapp/ui/search/CitySearchActivity.kt
@@ -0,0 +1,12 @@
+package com.mohdabbas.weatherapp.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohdabbas.weatherapp.R
+
+class CitySearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_search)
+    }
+}
